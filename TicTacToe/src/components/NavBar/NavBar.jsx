7a112c9f458338bf10501c08b94a00594e34a9d0
Vaibhav <vaibhavsingh2GMAIL.COM>
2024-05-22 17:53:50 +0530

    fixed navbar

--- TicTacToe/src/components/NavBar/NavBar.jsx
+++ TicTacToe/src/components/NavBar/NavBar.jsx
@@ -3,6 +3,10 @@ import './NavBar.css'
 import { Link } from "react-router-dom";
 
 function NavBar (where) {
+    const handleRefresh = (event) => {
+        event.preventDefault();
+        window.location.reload();
+      };
     console.log(where)
   return (
     <div className="nav-container">
@@ -12,8 +16,8 @@ function NavBar (where) {
           <nav>
             <ul class="nav-type">
               <li><Link to={'/'}>Home</Link></li>
-              <li>{(where.where == "computer") ? <Link to={'/player'}>2 Player</Link> : <Link to={'/select'}>Computer</Link> } </li>
-              <li><a>Restart</a></li>
+              <li>{(where.where == "computer") ? <Link to={'/player'}>vs Human</Link> : <Link to={'/select'}>vs Computer</Link> } </li>
+              <li><a onClick={handleRefresh}  >Restart</a></li>
             </ul>
           </nav>
         </div>
